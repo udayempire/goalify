e3bed093bfbc91496f1124a4fedbf4d7601c9003
Uday Kumar <kuday0726@gmail.com>
2025-08-15 15:51:29 +0000

    join goal function added

--- anchor-program/programs/goal_manager/src/instructions/join_goal.rs
+++ anchor-program/programs/goal_manager/src/instructions/join_goal.rs
@@ -1,3 +1,6 @@
+
+#[allow(deprecated)]
+#[allow(unexpected_cfgs)]
 use anchor_lang::prelude::*;
 use crate::state::{Goal, GoalParticipant, GoalStatus};
 use crate::errors::GoalError;
@@ -24,4 +27,24 @@ pub struct JoinGoal<'info>{
 
 }
 
+pub fn handler(ctx: Context<JoinGoal>, stake_amount: u64) -> Result<()> {
+    let goal = &mut ctx.accounts.goal;
+    
+    //ensurig goal is open
+    require!(
+        goal.status == GoalStatus::Pending,
+        GoalError::GoalNotOpen
+    );
+
+    //Save participant info
+    let goalParticipant = &mut ctx.accounts.goal_participant;
+    goalParticipant.goal = goal.key(); //goal pda pubkey
+    goalParticipant.participant = ctx.accounts.participant.key();
+    goalParticipant.stake_amount = stake_amount;
+
+    //update total stake
+    let new_total = goal.total_stake.checked_add(stake_amount).ok_or(GoalError::StakeOverflow)?;
+    goal.total_stake = new_total;
+    Ok(())
+}
 
