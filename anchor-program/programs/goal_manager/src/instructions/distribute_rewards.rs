d2799b9b161ef0a54518d2703407b91dabcf0de4
www.udayempire <147295044+udayempire@users.noreply.github.com>
2025-08-16 07:17:56 +0530

    added distribute_goals

--- anchor-program/programs/goal_manager/src/instructions/distribute_rewards.rs
+++ anchor-program/programs/goal_manager/src/instructions/distribute_rewards.rs
@@ -1,10 +1,7 @@
-use std::task::Context;
-
 #[allow(deprecated)]
 #[allow(unexpected_cfgs)]
-use anchor_lang::prelaude::*;
-use crate::state::{ Goal,GoalParticipant };
-use crate::errors::GoalError;
+use anchor_lang::prelude::*;
+use crate::state::{ Goal};
 
 #[derive(Accounts)]
 
@@ -17,15 +14,14 @@ pub struct DistributeRewards<'info> {
     pub goal: Account<'info,Goal>,
 
     //creator can trigger distribuition
-    pub authority: Signer<'info, Goal>
-
+    pub authority: Signer<'info>,
     pub system_program: Program<'info, System>
 
 }
 
 pub fn hander(ctx: Context<DistributeRewards>) -> Result<()>{
     let goal = &mut ctx.accounts.goal;
-    require!(goal.status===GoalStatus::Completed, GoalError::Not Completed);
+    require!(goal.status == GoalStatus::Completed, GoalError::NotCompleted);
     msg!(
         "Distributing {} lamports worth of rewards for goal: {}",
         goal.total_stake,
